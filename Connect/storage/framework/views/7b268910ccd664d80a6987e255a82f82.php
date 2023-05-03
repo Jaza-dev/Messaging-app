fd2986a3df4b519cd02eb4768e82384a3db4cdce
Zeljko Jazarevic <76903823+Jaza-dev@users.noreply.github.com>
2023-05-03 22:28:13 +0200

    Add files via upload

---
+++ Connect/storage/framework/views/7b268910ccd664d80a6987e255a82f82.php
@@ -0,0 +1,27 @@
+
+
+<?php $__env->startSection('title', 'My Posts'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <ul type="none">
+                <h3 class="text-white text-center">My posts</h3><br>
+                <?php $__currentLoopData = $myPosts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li class="p-3 bg-opacity-10 border border-secondary border-start-4 rounded">
+                        <h2 class="text-white"><?php echo e($post->title); ?></h2>
+                        <span class="text-white">Posted by: <?php echo e(auth()->user()->username); ?></span><br>
+                        <span class="text-white">Posted at: <?php echo e($post->posted_at); ?></span><br><hr>
+                        <p class="text-white"><?php echo e(wordwrap($post->text , 20, "\n", true)); ?></p><hr class="text-white">
+                        <span class="text-white">Likes:</span> <span class="text-white"><?php echo e($post->likes); ?></span><br>
+                        <span class="text-white">Dislikes:</span> <span class="text-white"><?php echo e($post->dislikes); ?></span><br><br>
+                        <a href="<?php echo e(route('showEditPostForm', ['idPost' => $post->idPost])); ?>" class="btn btn-warning"><i class="bi bi-gear"></i> Edit</a>
+                        <a href="<?php echo e(route('deletePost', ['idPost' => $post->idPost, 'idUser' => $post->idUser])); ?>" class="btn btn-danger position-relative"><i class="bi bi-trash"></i></a>
+                    </li><br>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('logged_user_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\jazar\zeljko-project\resources\views/myPosts.blade.php ENDPATH**/ ?>
---
+++ Connect/storage/framework/views/b7745bb9a7ef027dbb224b5cd32cd477.php
@@ -0,0 +1,18 @@
+
+
+<?php $__env->startSection('title', 'Create new post'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <br>
+    <div class="row">
+        <div class="col-12">
+            <form action="<?php echo e(route('createNewPost')); ?>" method="get">
+                <input type="hidden" name="idUser" value="<?php echo e(session('user')->idUser); ?>">
+                <input type="text" name="title" placeholder="Title"><br><br>
+                <textarea name="text" cols="23" rows="10" placeholder="Text..."></textarea><br><br>
+                <input class="btn btn-success" type="submit" value="Post">
+            </form>
+        </div>
+    </div class="row">
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('logged_user_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\jazar\zeljko-project\resources\views/showCreateNewPostForm.blade.php ENDPATH**/ ?>
